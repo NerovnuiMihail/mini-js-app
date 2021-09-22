417ba5c6c7beabd314b18fcc852754ae7925a9a8
NerovnuiMihail <79864794+NerovnuiMihail@users.noreply.github.com>
2021-09-22 12:07:09 +0300

    add first css

---
+++ constants/catalog.js
@@ -0,0 +1,57 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Ibanez GRX40-MLB',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/aa3/500_500_0/z81srqfzgcxt71281970067ohr3df84q.jpg',
+        price: '19200',
+    },
+    {
+        id: 'el2',
+        name: 'banez RGRT421-WK',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/9d4/500_500_0/ym5t00uz99zi8fsw03gbgh04l7a2y4py.jpg',
+        price: '44000',
+    },
+    {
+        id: 'el3',
+        name: 'Fender Squier Bullet Stratocaster Trem HSS - BSB',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/5f1/500_500_0/gqtxui4p8gvsu8236no2tdo5ceadx7sp.jpg',
+        price: '18800',
+    },
+    {
+        id: 'el4',
+        name: 'Fender Squier Bullet Mustang HH Imperial Blue',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/3d8/500_500_0/zslumfx9myx51c0ajg1mnjg420ax9anc.jpg',
+        price: '19500',
+    },
+    {
+        id: 'el5',
+        name: 'Homage HEG310BK',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/88c/500_500_0/se908a40uyjljs7g9fadlbe8wsb5twvn.jpg',
+        price: '10360',
+    },
+    {
+        id: 'el6',
+        name: 'Yamaha Pacifica012 RM',
+        img: 'https://guitar-guitar.ru/upload/iblock/0a6/h7lodtd7mern9wkl6e17qdkd0kkl7wtq.jpg',
+        price: '19990',
+    },
+    {
+        id: 'el7',
+        name: 'Yamaha Pacifica112JL BL',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/e01/500_500_0/6vuqt9nefpj3rz50ggun9agblehny658.jpg',
+        price: '29990',
+    },
+
+    {
+        id: 'el8',
+        name: 'Caraya E226N',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/667/500_500_0/oyrornfdpgika2bbndz6fdbsvndxpvnr.jpg',
+        price: '14970',
+    },
+    {
+        id: 'el9',
+        name: 'Fender Squier Affinity Telecaster Deluxe LRL Charcoal Frost Metallic',
+        img: 'https://guitar-guitar.ru/upload/resize_cache/iblock/7e3/500_500_0/dswnui2ce8atzt7om1ze4ua3iqg17mrj.jpg',
+        price: '31990',
+    }
+];
